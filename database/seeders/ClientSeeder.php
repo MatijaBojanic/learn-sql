31b1d649b19de0958c7d994640be8bb373ab2d77
MatijaBojanic <matija.bojanic@gmail.com>
2023-01-22 14:28:26 +0100

    Init project

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
